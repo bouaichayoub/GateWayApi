db69d3f6fc2c8312ec87c55111640f6a2a584339
Bouaich <ayoub.bouaich@gmail.com>
2021-12-24 12:54:57 +0100

    first commit

---
+++ src/main/java/com/cap/gate/config/GateWayApiConfig.java
@@ -0,0 +1,19 @@
+package com.cap.gate.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GateWayApiConfig {
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("commande-service", r -> r.path("/commande/**")
+                        .uri("lb://MS-COMMANDE"))
+                .route("paiement-service", r -> r.path("/paiement/**")
+                        .uri("lb://MS-PAIEMENT"))
+                .build();
+    }
+}
